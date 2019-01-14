12dfd1ecd22eee8b04236aedaa3221684dadef04
Brad <PoYi.Lee@yuanta.com>
2019-01-14 15:15:15 +0800

    add promise method in AsyncPractice

--- AsyncPractice.js
+++ AsyncPractice.js
@@ -1,18 +1,32 @@
 function logAfter(value, second) {
     return new Promise((resolve, reject) => {
         setTimeout(() => {
-            console.log(value)
-            resolve()
+                console.log(value)
+                resolve()
             }
             , second)
     })
 }
 
-async function startTask() {
+async function startAsyncTask() {
     await logAfter('2000', 2000)
     await logAfter('1000', 1000)
     await logAfter('500', 500)
     await logAfter('3000', 3000)
+}
+
+function startPromiseTask() {
+    logAfter('2000', 2000)
+        .then(logAfter.bind(null, '1000', 1000))
+        .then(logAfter.bind(null, '500',500))
+        .then(logAfter.bind(null, '3000',3000))
+}
+
+async function startTask() {
+    console.log('start async task')
+    await startAsyncTask()
+    console.log('start promise task')
+    startPromiseTask()
 }
 
 startTask()
